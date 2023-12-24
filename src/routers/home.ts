d735dc9650a35e4cc782cfc22deb3cb5a0f14ee5
jean carlos <jeancarlos3102@gmail.com>
2023-12-23 19:06:07 -0500

    delete params

--- src/routers/home.ts
+++ src/routers/home.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Router } from "express";
+import express, {Router } from "express";
 import { HomeController } from "../controllers/HomeController";
 
 export class HomeRouter {
